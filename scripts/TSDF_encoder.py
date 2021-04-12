da0177ab0b99327ea02949666010e464fb35152e
Martin Aalby Svalesen <martiasv@stud.ntnu.no>
2021-04-12 09:44:13 +0200

    Now possible to visualize 2D-slice in real time

--- scripts/TSDF_encoder.py
+++ scripts/TSDF_encoder.py
@@ -2,14 +2,16 @@
 
 #   <!-- TSDF_encoder -->
 #   <node name="TSDF_encoder" pkg="pointcloud_utils" type="TSDF_encoder.py" output="screen">
-#     <param name="reconstruct_TSDF" value="false"/>
-#     <param name="model_weight_path" value="$(find pointcloud_utils)/saved_model_weights/latent_dim_100/08-04_16_05/epoch_0055/cp-.ckpt"/>
+#     <param name="reconstruct_TSDF" value="true"/>
+#     <param name="publish slice" value="true"/> <!-- reconstruct tsdf needs to be true for this to take effect -->
+#     <param name="model_weight_path" value="$(find pointcloud_utils)/saved_model_weights/latent_dim_100/10-04_16_39/epoch_0064/cp-.ckpt"/>
 #    </node>
 
 import sensor_msgs.point_cloud2 as pc2
 import ros_numpy as ros_np
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import PointField
+from sensor_msgs.msg import Image
 import rospy
 import numpy as np
 import time
@@ -25,9 +27,11 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from pointcloud_utils.msg import LatSpace
 import os
+import cv2
+from cv_bridge import CvBridge
 
 class unordered_pointcloud_to_latent_space():
-    def __init__(self,pc_topic="/gagarin/tsdf_server/tsdf_pointcloud",lat_topic="/gagarin/pc_latent_space",recon_topic="/gagarin/reconstructed_pc"):
+    def __init__(self,pc_topic="/gagarin/tsdf_server/tsdf_pointcloud",lat_topic="/gagarin/pc_latent_space",recon_topic="/gagarin/reconstructed_pc",slice_pub="/gagarin/pc_slice"):
 
         #Import model weight path
         self.arg_filepath = rospy.get_param("~model_weight_path")
@@ -50,6 +54,11 @@ class unordered_pointcloud_to_latent_space():
         else:
             self.reconstruct = False
 
+        if rospy.get_param("~publish_slice"):
+            self.pub_slice = True
+            self.slice_pub = rospy.Publisher(slice_pub,Image,queue_size=1)
+        else:
+            self.pub_slice = False
 
 
 
@@ -97,7 +106,7 @@ class unordered_pointcloud_to_latent_space():
             #Shift points to that the origin matches
             R = R - (scale_factor*27)
             C = C - (scale_factor*27)
-            T = T - (scale_factor*10)
+            T = T - (scale_factor*8)
 
             #Create fields
             fields = [PointField('x', 0, PointField.FLOAT32, 1),
@@ -119,6 +128,12 @@ class unordered_pointcloud_to_latent_space():
             #Publish pointcloud
             self.pc_pub.publish(output_pc)
             rospy.loginfo("Published Encoded-Decoded pointcloud")
+
+            if self.pub_slice == True:
+                image_temp  = CvBridge().cv2_to_imgmsg(cropped_output_image[:,:,8])
+                image_temp.header = header
+                self.slice_pub.publish(image_temp)
+                rospy.loginfo("Published Encoded-Decoded image slice")
 
 
 
